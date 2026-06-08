8bb74fe4e2b4ac3973467c3e86f3a93cfa8090cd
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-07 21:17:38 -0700

    virtualpad

--- source/funkin/options/TreeMenu.hx
+++ source/funkin/options/TreeMenu.hx
@@ -9,8 +9,15 @@ import flixel.util.typeLimit.OneOfTwo;
 import funkin.options.type.OptionType;
 import funkin.options.type.TextOption;
 import funkin.backend.system.framerate.Framerate;
+#if mobile
+import mobile.controls.VirtualPad;
+import mobile.controls.FlxButton;
+#end
 
 class TreeMenu extends UIState {
+	#if mobile
+    public var virtualPad:VirtualPad;
+    #end
 	public var main:OptionsScreen;
 	public var optionsTree:OptionsTree;
 	public var pathLabel:FunkinText;
